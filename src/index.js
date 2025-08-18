710ccb702b58a82d770b8aa739b45713649ba7fb
Jagadish-shinde <jagdish.shinde@polariscampus.com>
2025-08-18 18:10:23 +0530

    add default port 3000

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 // Global middlewares
 app.use(cors());
